c9149f5cea2c42abd9c16b30845f34be212c705f
ioannisgeorvasilis <jhngeorvasilis@gmail.com>
2022-01-06 20:07:30 +0200

    ae-cnn module defined

--- models/ae-cnn/cnn.py
+++ models/ae-cnn/cnn.py
@@ -2,21 +2,21 @@ import argparse
 import torch.nn as nn
 import torch.nn.functional as F
 
+from convblock import ConvBlock
 
 parser = argparse.ArgumentParser(description='Convolutional Neural Network')
-parser.add_argument('--batch-size', type=int, default=64,
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--test-batch-size', type=int, default=64,
-                    help='input batch size for testing (default: 64)')
-parser.add_argument('--epochs', type=int, default=10, 
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.01, 
-                    help='learning rate (default: 0.01)')
-parser.add_argument('--cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--dry-run', action='store_true', default=False,
-                    help='quickly check a single pass')
-
+parser.add_argument('--IN_CHANNELS', type=int, default=64,
+                    help='num of input channels (default: 2048)')
+parser.add_argument('--OUT_CHANNELS', type=int, default=64,
+                    help='num of output channels (default: 3)')
+parser.add_argument('--N_CONVBLOCKS', type=int, default=64,
+                    help='num of convblocks of encoder and decoder (default: 4)')
+parser.add_argument('--KERNEL_SIZE', type=int, default=64,
+                    help='size of kernel of convolutional operation (default: 3)')
+parser.add_argument('--STRIDE', type=int, default=64,
+                    help='stride of convolutional operation (default: 1)')
+parser.add_argument('--PADDING', type=int, default=64,
+                    help='padding of convolutional operation (default: 0)')
 
 class Net(nn.Module):
     def __init__(self, 
@@ -24,11 +24,22 @@ class Net(nn.Module):
                 OUT_CHANNELS = 3,
                 N_CONVBLOCKS = 4,
                 KERNEL_SIZE = 3,
-                STRIDE = 1 ):
+                STRIDE = 1 ,
+                PADDING = 0):
         
         super(Net, self).__init__()
-        x= 1
+        self.IN_CHANNELS = IN_CHANNELS
+        self.OUT_CHANNELS = OUT_CHANNELS
+        self.N_CONVBLOCKS = N_CONVBLOCKS
+        self.KERNEL_SIZE = KERNEL_SIZE
+        self.STRIDE = STRIDE
+        self.PADDING = PADDING
+
+        self.convBlocks = list()
         
+        # Building encoder
+        for i in range(N_CONVBLOCKS):
+            self.e1 = 1        
         # self.conv1 = nn.Conv2d(1, 32, 3, 1)
         # self.conv2 = nn.Conv2d(32, 64, 3, 1)
         # self.dropout1 = nn.Dropout(0.25)
---
+++ models/ae-cnn/convblock.py
@@ -0,0 +1,41 @@
+import argparse
+import torch
+import torch.nn as nn
+
+from utils import getFunction
+
+class ConvBlock(nn.Module):
+    def __init__(self, 
+        IN_CHANNELS,
+        OUT_CHANNELS,
+        KERNEL_SIZE = 3,
+        STRIDE = 1 ,
+        PADDING = 0, 
+        ACTIVATE_FUNC = "relu",
+        USE_BATCHNORM = True ):
+            
+        super().__init__()
+
+        self.conv = nn.Conv2d(IN_CHANNELS, 
+                              OUT_CHANNELS.
+                              KERNEL_SIZE,
+                              STRIDE, 
+                              PADDING, 
+                              bias=False)
+        
+        self.f = getFunction("relu")
+        
+        self.bn = USE_BATCHNORM
+        if USE_BATCHNORM: 
+            self.bn = nn.BatchNorm2d(OUT_CHANNELS)
+        
+
+    def forward(self, x):
+        out = self.conv(x)
+        
+        if self.USE_BATCHNORM:
+            out = self.bn(out)
+            
+        out = self.f(out)
+
+        return out
