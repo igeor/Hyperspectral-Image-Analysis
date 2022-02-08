04d4dd5fb0d557dc53d595fe192137a8111e1d2a
ioannisgeorvasilis <ioannis.georvasilis@gmail.com>
2022-02-08 16:29:26 +0200

    spec2spec pixel2pixel

--- spec2rgb/trainSpec2Spec.py
+++ spec2rgb/trainSpec2Spec.py
@@ -1,152 +1,115 @@
-import os
 
 import torch
 import torchvision
-from torch import nn
-from torch.autograd import Variable
-from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
-from torchvision.datasets import MNIST
-from torchvision.utils import save_image
 import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+from torch.utils.data.dataset import Dataset  # For custom datasets
+from torch.utils.data import DataLoader
+from torchvision.utils import save_image
 from PIL import Image
-
 from __init__ import *
 
-def h5toNumpy(filepath):
-    f = h5py.File(filepath, 'r')
-    image = np.array(f['dataset'])
-    energies = np.array(f['energies'])
-    f.close()
-    return image
+device = "cpu"
+
+f = h5py.File(parentdir + "\\" + "data\panagia.h5",  'r')
+dset = f['dataset']
+energies = f['energies']
+spectra_panagia = np.array(dset)
+spectra_panagia = spectra_panagia[:,60:900]
+energies = np.array(energies)
+f.close()
 
 
+import torchvision.transforms.functional as TF
 
-def saveImage(image, name):
-    if(type(image) == type(torch.zeros((1,2)))):
-        plt.imshow(image, interpolation='nearest')
-    else:
-        plt.imshow(image.reshape(image.shape[1],image.shape[2], image.shape[0]), interpolation='nearest')
-    plt.savefig(name)
+inTrainImage = spectra_panagia.astype(np.float32)
+inTrainImage = torch.from_numpy(inTrainImage)
+inTrainImage = torch.reshape(inTrainImage, (21,33,840))
+inTrainImage = torch.swapaxes(inTrainImage,0,2);	inTrainImage = np.swapaxes(inTrainImage,1,2)
+inTrainImage = torch.rot90(inTrainImage, 2, (2,1))
+inTrainImage = torch.reshape(inTrainImage, (840, 21 * 33))
+print(inTrainImage.shape)
 
-
-num_epochs = 100
-batch_size = 128
-learning_rate = 1e-3
-
-img_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize([0.5], [0.5])
-])
+outTrainImage = TF.to_tensor(Image.open(parentdir + "\\" + "data\panagia.png"))[:3,:,:]
+outTrainImage = torch.reshape(outTrainImage, (3, 21 * 33))
+print(outTrainImage.shape)
 
 
-
-class CustomDataset(Dataset):
-        def __init__(self):
-            self.train = True 
-
-            self.inTrainImagePath = parentdir + "\\" + "data\jesus.h5"
-            self.outTrainImagePath = parentdir + "\\" + "data\jesus.png"
-            self.inTestImagePath = parentdir + "\\" + "data\panagia.h5"
-            self.outTestImagePath = parentdir + "\\" + "data\panagia.png"
-            self.h5TrainShape = (31, 46, 2048)
-            self.h5TestShape = (21, 33, 2048)
-
-            self.inTrainImage = h5toNumpy(self.inTrainImagePath).astype(np.float32)
-            self.inTrainImage = torch.from_numpy(self.inTrainImage)
-            self.inTrainImage = torch.reshape(self.inTrainImage, (31,46,2048))
-            self.inTrainImage = torch.swapaxes(self.inTrainImage,0,2);	self.inTrainImage = np.swapaxes(self.inTrainImage,1,2)
-            self.inTrainImage = torch.rot90(self.inTrainImage, 2, (2,1))
-            self.pltInTrainImage = torch.sum(self.inTrainImage, axis=0)
-            self.inTrainImage = self.inTrainImage.reshape(2048, 31 * 46)  
-
-            self.inTestImage = h5toNumpy(self.inTestImagePath).astype(np.float32)
-            self.inTestImage = torch.from_numpy(self.inTestImage)
-            self.inTestImage = torch.reshape(self.inTestImage, (21,33,2048))
-            self.inTestImage = torch.swapaxes(self.inTestImage,0,2);	self.inTestImage = np.swapaxes(self.inTestImage,1,2)
-            self.inTestImage = torch.rot90(self.inTestImage, 2, (2,1))
-            self.pltInTestImage = torch.sum(self.inTestImage, axis=0)
-            self.inTestImage = self.inTestImage.reshape(2048, 21 * 33)
-
-            self.outTestImage = torch.from_numpy(np.array(Image.open(self.outTestImagePath).convert('RGB')).flatten().reshape(3, 21 * 33))
-            self.outTrainImage = torch.from_numpy(np.array(Image.open(self.outTrainImagePath).convert('RGB')).flatten().reshape(3, 31 * 46))
-            # print(self.inTrainImage.shape)
-            # print(self.inTestImage.shape)
-            # print(self.outTestImage.shape)
-            # print(self.outTrainImage.shape)
-        
-        def setTrain(self, train):
-            self.train = train
-
-        def __len__(self):
-            if(not self.train):
-                return self.inTestImage.shape[-1]
-            return self.inTrainImage.shape[-1]
-        
-        def __getitem__(self, index):
-            if(not self.train):
-                return self.inTestImage[:,index], self.outTestImage[:, index]
-            return self.inTrainImage[:, index], self.outTrainImage[:, index]
-
-
-dataset = CustomDataset()
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
-
-class autoencoder(nn.Module):
+class AutoEncoder(nn.Module):
     def __init__(self):
-        super(autoencoder, self).__init__()
-
+        super(AutoEncoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Linear(2048, 512),
-            nn.ReLU(True),
-            nn.Linear(512, 256),
-            nn.ReLU(True),
-            nn.Linear(256, 128),
-            nn.ReLU(True),
-            nn.Linear(128, 64),
-            nn.ReLU(True), 
-            nn.Linear(64, 12), 
-            nn.ReLU(True),
-            nn.Linear(12, 3)
+                nn.Linear(840, 512),
+                nn.ReLU(),
+                nn.Linear(512, 256),
+                nn.ReLU(),
+                nn.Linear(256, 64),
+                nn.ReLU(),
+                nn.Linear(64, 3)
         )
-
         self.decoder = nn.Sequential(
-            nn.Linear(3, 12),
-            nn.ReLU(True),
-            nn.Linear(12, 64),
-            nn.ReLU(True),
-            nn.Linear(64, 128),
-            nn.ReLU(True),
-            nn.Linear(128, 256),
-            nn.ReLU(True),
-            nn.Linear(256, 512), 
-            nn.ReLU(True),
-            nn.Linear(512, 2048)
+                nn.Linear(3, 64),
+                nn.ReLU(),
+                nn.Linear(64, 256),
+                nn.ReLU(),
+                nn.Linear(256, 512),
+                nn.ReLU(),
+                nn.Linear(512, 840)
         )
 
-    def forward(self, x):
-        x_enc = self.encoder(x)
-        x_dec = self.decoder(x_enc)
-        return x_enc, x_dec
+    def forward(self, pixel):
+        encoded = self.encoder(pixel)
+        decoded = self.decoder(encoded)
+        return encoded, decoded
 
 
-model = autoencoder()
-criterion = nn.L1Loss()
-optimizer = torch.optim.Adam(
-    model.parameters(), lr=0.02, weight_decay=1e-5)
 
-for epoch in range(num_epochs):
-    for spec, rgb in dataloader:
+# hyperparameters
+N_EPOCH = 100
+BATCH_SIZE = 1
+DISPLAY_STEP = 200
+learning_rate = 0.0002
+N_PIXEL = 693
+PATH = "model.pt"
+
+
+autoencoder = AutoEncoder().to(device)
+optimizer = torch.optim.Adam(autoencoder.parameters(), lr=learning_rate)
+loss_fn = nn.L1Loss()
+
+
+out_img = torch.zeros(3,21,33)
+min_total_loss = float('inf')
+total_loss = 0
+
+for epoch in range(N_EPOCH):
+    for i in range(N_PIXEL):
+        pixel = inTrainImage[:,i]
+        pixel_real = outTrainImage[:,i]
+        (encoded, decoded) = autoencoder(pixel)
+
+        total_loss += loss_fn(decoded, pixel) 
+
+        if(i % DISPLAY_STEP == 0 and i > 0):
+            print("epoch ",str(epoch)+": "+str(total_loss.item()))#, end="\r")
+            #save_image(out_img, "results/" + str(epoch)+"_"+str(i)+"_"+'out.png')
+
+                
+        if(i % BATCH_SIZE == 0 and i > 0):
+            optimizer.zero_grad()
+            total_loss.backward()
+            optimizer.step()
+            total_loss = 0
         
-        # ===================forward=====================
-        enc, dec = model(spec)
-        loss = criterion(dec, spec)
-        # ===================backward====================
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+       
 
-        # ===================log========================
-        print('epoch [{}/{}], loss:{:.4f}'
-            .format(epoch + 1, num_epochs, loss))
+        r = i // 33 ; c = i % 33
+        #print(r,c)
+        out_img[:, r, c] = encoded
+        save_image(out_img, 'out.png')
+        
         
