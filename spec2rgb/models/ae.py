ddb249ff2b306b981a270a407719e5559da689ca
ioannisgeorvasilis <jhngeorvasilis@gmail.com>
2022-02-12 15:39:59 +0200

    spec2spec refactored

--- spec2rgb/models/ae.py
+++ spec2rgb/models/ae.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class Encoder(nn.Module):
@@ -6,30 +7,43 @@ class Encoder(nn.Module):
         self.l1 =  nn.Sequential(
             nn.Linear(in_channels, in_channels//2),
             nn.ReLU()
+            #nn.BatchNorm1d(in_channels//2)
         )
         self.l2 =  nn.Sequential(
             nn.Linear(in_channels//2, in_channels//4),
             nn.ReLU()
+            #nn.BatchNorm1d(in_channels//4)
         )
         self.l3 =  nn.Sequential(
             nn.Linear(in_channels//4, in_channels//8),
             nn.ReLU()
+            #nn.BatchNorm1d(in_channels//8)
         )
         self.l4 =  nn.Sequential(
             nn.Linear(in_channels//8, in_channels//16),
             nn.ReLU()
+            #nn.BatchNorm1d(in_channels//16)
         )
         self.l5 =  nn.Sequential(
             nn.Linear(in_channels//16, out_channels),
         )
 
-    def forward(self, x_in):
+    def forward(self, x_in, train=True):
         x1 = self.l1(x_in)
-        x2 = self.l2(x1)
-        x3 = self.l3(x2)
+        #print(x1.shape)
+        x2 = self.l2(x1); 
+        if(train):
+            x2 == nn.Dropout(0.2)
+        #print(x2.shape)
+        x3 = self.l3(x2); 
+        if(train):
+            x2 == nn.Dropout(0.2)
+        #print(x3.shape)
         x4 = self.l4(x3)
+        #print(x4.shape)
         x_enc = self.l5(x4)
         return x1,x2,x3,x4,x_enc 
+
 
 class Decoder(nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -37,27 +51,39 @@ class Decoder(nn.Module):
         self.l1 =  nn.Sequential(
             nn.Linear(in_channels, out_channels//16),
             nn.ReLU()
+            #nn.BatchNorm2d(out_channels//16)
         )
         self.l2 =  nn.Sequential(
             nn.Linear(out_channels//16, out_channels//8),
             nn.ReLU()
+            #nn.BatchNorm1d(out_channels//8)
         )
         self.l3 =  nn.Sequential(
             nn.Linear(out_channels//8, out_channels//4),
             nn.ReLU()
+            #nn.BatchNorm1d(out_channels//4)
         )
         self.l4 =  nn.Sequential(
             nn.Linear(out_channels//4, out_channels//2),
             nn.ReLU()
+            #nn.BatchNorm1d(out_channels//2)
         )
         self.l5 =  nn.Sequential(
             nn.Linear(out_channels//2, out_channels),
         )
 
-    def forward(self, x1_e,x2_e,x3_e,x4_e,x_in):
+    def forward(self, x1_e,x2_e,x3_e,x4_e,x_in, train=True):
         x1 = self.l1(x_in)
-        x2 = self.l2(x1 + x4_e)
-        x3 = self.l3(x2 + x3_e)
+        #print(x1.shape)
+        x2 = self.l2(x1 + x4_e); 
+        if(train):
+            x2 == nn.Dropout(0.2)
+        #print(x2.shape)
+        x3 = self.l3(x2 + x3_e); 
+        if(train):
+            x2 == nn.Dropout(0.2)
+        #print(x3.shape)
         x4 = self.l4(x3 + x2_e)
+        #print(x4.shape)
         x_dec = self.l5(x4)
         return x_dec 
--- spec2rgb/trainSpec2Spec.py
+++ spec2rgb/trainSpec2Spec.py
@@ -6,20 +6,36 @@ import torchvision.transforms.functional as TF
 from torchvision.utils import save_image
 from PIL import Image
 from models import Encoder, Decoder
+import atexit
 
 from __init__ import *
 
+
+def PSNR(img1, img2):
+    mse = torch.mean((img1 - img2) ** 2)
+    return 20 * torch.log10(255.0 / torch.sqrt(mse))
+    
+parser = argparse.ArgumentParser()
+parser.add_argument("-fout", "--outfilename", default='out')
+parser.add_argument("-bs", "--batchsize", default=32)
+parser.add_argument("-lr", "--lrate", default=0.0002)
+parser.add_argument("-ep", "--epochs", default=100)
+parser.add_argument("-tr", "--ntrain", default=10)
+args = parser.parse_args()
+
 device = "cpu"
 if(torch.cuda.is_available()):
+    print('---device:cuda---')
     device = "cuda"
-    
+else:
+    print('---device:cpu---')
+
 f = h5py.File(parentdir + "\\" + "data\panagia.h5", 'r')
 dset = f['dataset']
 energies = f['energies']
 spectra_panagia = np.array(dset)
 spectra_panagia = spectra_panagia[:, 60:900]
 energies = np.array(energies)
-print(energies,energies.shape);input()
 f.close()
 
 f2 = h5py.File(parentdir + "\\" + "data\jesus.h5", 'r')
@@ -37,6 +53,8 @@ inTrainImage = torch.swapaxes(inTrainImage, 0, 2)
 inTrainImage = np.swapaxes(inTrainImage, 1, 2)
 inTrainImage = torch.rot90(inTrainImage, 2, (2, 1))
 inTrainImage = torch.reshape(inTrainImage, (840, 21 * 33))
+inTrainImageMean = torch.mean(inTrainImage, dim=1)
+inTrainImageStd = torch.std(inTrainImage, dim=1)
 # print(inTrainImage.shape)
 
 outTrainImage = TF.to_tensor(Image.open(parentdir + "\\" + "data\panagia.png"))[:3, :, :]
@@ -50,91 +68,112 @@ inTestImage = torch.swapaxes(inTestImage, 0, 2)
 inTestImage = np.swapaxes(inTestImage, 1, 2)
 inTestImage = torch.rot90(inTestImage, 2, (2, 1))
 inTestImage = torch.reshape(inTestImage, (840, 31 * 46))
+inTestImageMean = torch.mean(inTestImage, dim=1)
+inTestImageStd = torch.std(inTestImage, dim=1)
+# print(inTrainImage.shape)
 # print(inTestImage.shape)
 
 outTestImage = TF.to_tensor(Image.open(parentdir + "\\" + "data\jesus.png"))[:3, :, :]
-outTestImage = torch.reshape(outTestImage, (3, 31 * 46))
+outTestImage_flat = TF.to_tensor(Image.open(parentdir + "\\" + "data\jesus.png"))[:3, :, :]
+outTestImage = torch.reshape(outTestImage_flat, (3, 31 * 46))
 # print(outTestImage.shape)
 
 # hyperparameters
-N_EPOCH = 300
-BATCH_SIZE = 33
+int(args.batchsize)
 DISPLAY_STEP = 200
-learning_rate = 0.00002
+float(args.lrate) #= 0.00002
 N_PIXEL = 21 * 33
 TEST_PIXEL = 31 * 46
 PATH = "model.pt"
 
-encoder = Encoder(840,3).to(device)
-enc_optimizer = torch.optim.Adam(encoder.parameters(), lr=learning_rate)
-decoder = Decoder(3,840).to(device)
-dec_optimizer = torch.optim.Adam(decoder.parameters(), lr=learning_rate)
-
-L1Loss = nn.L1Loss()
-L2Loss = nn.MSELoss()
-
-for epoch in range(N_EPOCH):
+avg_psnr_metric = 0
+total_max_psnr_metric = 0
+for i_train in range(int(args.ntrain)):
     
+    encoder = Encoder(840,3).to(device)
+    enc_optimizer = torch.optim.Adam(encoder.parameters(), lr=float(args.lrate))
+    decoder = Decoder(3,840).to(device)
+    dec_optimizer = torch.optim.Adam(decoder.parameters(), lr=float(args.lrate))
+
+    L1Loss = nn.L1Loss()
+    L2Loss = nn.MSELoss()
+
     ##### TRAINING #####
-    total_loss = 0
-    total_l2Loss_dec,total_l2Loss_enc = 0, 0
-    out_img1 = torch.zeros(3, 21, 33)
-    for i in range(0, N_PIXEL, BATCH_SIZE):
+    max_psnr_metric = -1
+    for epoch in range(int(args.epochs)):
+        
+        total_loss = 0
+        total_l2Loss_dec,total_l2Loss_enc = 0, 0
+        
+        for i in range(0, N_PIXEL, int(args.batchsize)):
 
-        pixel = inTrainImage[:, i:i+BATCH_SIZE].to(device) / max(energies)
-        pixel_real = outTrainImage[:, i:i+BATCH_SIZE].to(device)
+            pixel = inTrainImage[:, i:i+int(args.batchsize)].to(device) / max(energies)
+            pixel_real = outTrainImage[:, i:i+int(args.batchsize)].to(device)
 
-        pixel = torch.swapaxes(pixel, 0, 1)
-        pixel_real = torch.swapaxes(pixel_real, 0, 1)
+            pixel = torch.swapaxes(pixel, 0, 1)
+            pixel_real = torch.swapaxes(pixel_real, 0, 1)
 
-        ### Update Decoder ###
-        dec_optimizer.zero_grad()
-        with torch.no_grad():
+            ### Update Decoder ###
+            dec_optimizer.zero_grad()
+            with torch.no_grad():
+                x1,x2,x3,x4,encoded  = encoder(pixel)
+
+            decoded = decoder(x1,x2,x3,x4,encoded.detach())
+            dec_loss = L1Loss(decoded, pixel) 
+
+            ### Update Encoder ###
+            enc_optimizer.zero_grad()
             x1,x2,x3,x4,encoded  = encoder(pixel)
+            decoded = decoder(x1,x2,x3,x4,encoded)
+            enc_loss = L1Loss(decoded, pixel) 
+            enc_loss = L2Loss(encoded, pixel_real) * 100 # * (1 - epoch/int(args.epochs))* 100
+                
+            enc_loss.backward()
+            enc_optimizer.step()
+            dec_loss.backward()
+            dec_optimizer.step()
 
-        decoded = decoder(x1,x2,x3,x4,encoded.detach())
-        dec_loss = L1Loss(decoded, pixel)
-        #dec_loss += L2Loss(encoded.detach(), pixel_real) / (N_PIXEL // BATCH_SIZE)
-
-        ### Update Encoder ###
-        enc_optimizer.zero_grad()
-        x1,x2,x3,x4,encoded  = encoder(pixel)
-        decoded = decoder(x1,x2,x3,x4,encoded)
-        enc_loss = L1Loss(decoded, pixel) 
-        enc_loss += L2Loss(encoded, pixel_real) * (1 - epoch/N_EPOCH) * 100
-
-        
             
-        enc_loss.backward()
-        enc_optimizer.step()
-        dec_loss.backward()
-        dec_optimizer.step()
 
-        for x, j in enumerate(range(i, i+BATCH_SIZE)):
-            r = j // 33;c = j % 33
-            out_img1[:, r, c] = encoded[x]
+        ##### VALIDATING #####
+        out_img = torch.zeros(3, 31, 46)
+        for i in range(TEST_PIXEL):
+            pixel = inTestImage[:, i].to(device) / max(energies)
+            r = i // 46; c = i % 46
 
+            with torch.no_grad():
+                x1,x2,x3,x4,encoded = encoder(pixel, train=False)
+            
+            out_img[:, r, c] = encoded
+            
+        ##### SHOW CURRENT RESULTS #####    
+        psnr_metric = PSNR(out_img, outTestImage_flat)
+        if(psnr_metric.item() > max_psnr_metric):
+            max_psnr_metric = psnr_metric.item()
+            best_epoch = epoch
+            best_out_img = out_img
+            save_image(out_img, './results/spec2spec/best_'+
+                args.outfilename+'-'
+                'bs_'+str(int(args.batchsize))+'-'+
+                'lr_'+str(float(args.lrate))+'-'+
+                '.png')
         
-    ##### TESTING #####
-    TEST_BATCH_SIZE = 46
-    out_img2 = torch.zeros(3, 31, 46)
-    for i in range(0, TEST_PIXEL, TEST_BATCH_SIZE):
+        if(psnr_metric.item() > total_max_psnr_metric):
+            total_max_psnr_metric = psnr_metric.item()
+            total_best_out_img = out_img
 
-        pixel = inTestImage[:, i:i+TEST_BATCH_SIZE].to(device)  / max(energies)
-        pixel_real = outTrainImage[:, i:i+TEST_BATCH_SIZE].to(device)
-
-        pixel = torch.swapaxes(pixel, 0, 1)
-        pixel_real = torch.swapaxes(pixel_real, 0, 1)
-
-        with torch.no_grad():
-            x1,x2,x3,x4,encoded = encoder(pixel)
-
-        for x, j in enumerate(range(i, i+TEST_BATCH_SIZE)):
-            r = j // 46
-            c = j % 46
-            out_img2[:, r, c] = encoded[x]
-
-    ##### VIEW RESULTS #####
-    print("epoch ", str(epoch)+": "+str(dec_loss.item()), end="\r")
-    save_image(out_img1, './results/spec2spec/out1.png')
-    save_image(out_img2, './results/spec2spec/out2.png')
+        print(" |itrain "+str(i_train)+ 
+              " |epoch "+str(epoch)+
+              " |loss "+str(round(dec_loss.item(),6))+
+              " |psnr "+str(round(max_psnr_metric,2)), end="\r")
+    
+    print()
+    avg_psnr_metric += max_psnr_metric / int(args.ntrain)
+    
+print(" === AVERAGE_PSNR:"+str(round(avg_psnr_metric,2))+" ===")
+save_image(total_best_out_img, './results/spec2spec/best_'+
+                args.outfilename+'-'
+                'psnr_'+str(round(avg_psnr_metric, 2))+'-'+
+                'bs_'+str(int(args.batchsize))+'-'+
+                'lr_'+str(float(args.lrate))+'-'+
+                '.png')
